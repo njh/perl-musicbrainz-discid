7c5c7797e6175a2361bb230903478521c398d1d2
Gregor Herrmann <gregoa@cpan.org>
2016-02-06 04:59:39 +0100

    Fixed typo in POD (RT#85212)

--- lib/MusicBrainz/DiscID.pm
+++ lib/MusicBrainz/DiscID.pm
@@ -156,7 +156,7 @@ Returns a device string for the default device for this platform.
 Construct a new DiscID object.
 
 As an optional argument the name of the device to read the ID from may 
-be given. If you donÔt specify a device here you can later read the ID with 
+be given. If you don't specify a device here you can later read the ID with 
 the read method.
 
 =item $discid->error_msg()
